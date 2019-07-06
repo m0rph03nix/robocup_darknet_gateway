bf371b4c1aa14c5bae61d4fc352f426672e84e8d
Vincent LE DOZE <vincent.le-doze@inria.fr>
2019-07-06 07:40:34 +0200

    Added comments and explanations in the code + some refactoring

--- darknet_gateway_node/scripts/darknet_gateway.py
+++ darknet_gateway_node/scripts/darknet_gateway.py
@@ -5,127 +5,116 @@ __author__ ='Raphael Leber'
 import rospy
 import actionlib
 
-
-#from darknet_gateway_srvs.srv import OpenPoseGossip
-
-
-
+import cv2
+from cv_bridge import CvBridge
 
 from darknet_gateway_srvs.srv import ObjectsDetectionGateway_Srv as ODG_Srv
 from darknet_gateway_srvs.srv import ObjectsDetectionGateway_distSorted_Srv as ODG_distSorted_Srv
 
-from darknet_ros_msgs.msg import BoundingBox, BoundingBoxes, CheckForObjectsAction, CheckForObjectsGoal
-
-
-
-from robocup_msgs.msg import Entity2D, Entity2DList
+from darknet_ros_msgs.msg import CheckForObjectsAction, CheckForObjectsGoal
 from sensor_msgs.msg import Image
 
 from process.ObjectsDetectionGateway_process import ObjectsDetectionGateway_process as ODG_process
 
 
-
-
 class ObjectsDetectionGateway_node():
+    """
+    Gateway for object detection with darknet.
+    """
 
     def __init__(self):
-
-        self.ready = 0
+        """
+        Create and start a ROS node ObjectsDetectionGateway_node
+        """
         self.image_ready = 0
         self.image_width = 0
         self.image_height = 0
-
+        #Node initialisation
         rospy.init_node('ObjectsDetectionGateway_node', anonymous=False)
         rospy.loginfo("ObjectsDetectionGateway_node init")
-
-        #rospy.Subscriber("/pepper_robot/naoqi_driver/camera/front/image_raw", Image, self.img_callback)
-        self.IMAGE_TOPIC_NAME = rospy.get_param("/darknet_ros/subscribers/camera_reading/topic","/videofile/image_raw")
-
-        rospy.Subscriber(self.IMAGE_TOPIC_NAME, Image, self.img_callback)
-        rospy.loginfo("ObjectsDetectionGateway_node topics init")
-
-        #declare ros service
+        #Declare ros service
         self.opg_Srv = rospy.Service('object_detection_gateway_srv', ODG_Srv, self.ODG_BB_SrvCallback)
         rospy.loginfo("ObjectsDetectionGateway_node object_detection_gateway_srv services init")
-
         self.opg_distSorted_Srv = rospy.Service('object_detection_gateway_distSorted_srv', ODG_distSorted_Srv, self.ODG_BB_distSorted_SrvCallback )
         rospy.loginfo("ObjectsDetectionGateway_node object_detection_gateway_distSorted_srv services init")
-
         self._actCheckForObjects = actionlib.SimpleActionClient("/darknet_ros/check_for_objects", CheckForObjectsAction)
-        #self._actCheckForObjects.wait_for_server(rospy.Duration(5))
         self._actCheckForObjects.wait_for_server()
         rospy.loginfo("ObjectsDetectionGateway_node actions init")
-
-        self.service_running = 0
-        self.ready = 1
-
+        #Topic subscribing
+        self.image_topic_name = rospy.get_param("/darknet_ros/subscribers/camera_reading/topic","/videofile/image_raw")
+        rospy.Subscriber(self.image_topic_name, Image, self.img_callback)
+        rospy.loginfo("ObjectsDetectionGateway_node topics init")
+        #Node online
         rospy.spin()
 
+    def ODG_BB_SrvCallback(self, req):
+        """
+        Callback on object_detection_gateway_srv service
 
-
-    def ODG_BB_SrvCallback(self,req):
-
-        #self.service_running = 1
-        #self.image_ready = 0
-
-        odg_process = ODG_process()
-
+        Parameters
+        ----------
+        req : darknet_gateway_srvs.srv ObjectsDetectionGateway_Srv
+            Service request
+        """
         #Get image
         if req.img_file_path == "":
+            #Load from topic
             while not self.image_ready :
                 rospy.loginfo("wait image")
                 rospy.sleep(0.05)
             msg_img = self.msg_img
         else:
+            #Load from file
             img_loaded = cv2.imread(req.img_file_path)
-            msg_img = self._bridge.cv2_to_imgmsg(img_loaded, encoding="bgr8")
-
+            msg_img = CvBridge().cv2_to_imgmsg(img_loaded, encoding="bgr8")
         #Create Goal
         goal = CheckForObjectsGoal()
         goal.id = 1
         goal.image = msg_img
-
         #Send and wait for results
         self._actCheckForObjects.send_goal(goal)
         self._actCheckForObjects.wait_for_result()
         result = self._actCheckForObjects.get_result()
-
         #Get Entities from results
+        odg_process = ODG_process()
         e2D = odg_process.BoundingBoxes_to_Entity2DList( result.bounding_boxes, req.labels )
-
-        #self.service_running = 0
-
+        #Servie output
         return e2D
 
+    def ODG_BB_distSorted_SrvCallback(self, req):
+        """
+        Callback on object_detection_gateway_distSorted_srv service
 
-    def ODG_BB_distSorted_SrvCallback(self,req):
-        odg_process = ODG_process()
+        Parameters
+        ----------
+        req : darknet_gateway_srvs.srv ObjectsDetectionGateway_distSorted_Srv
+            Service request
+        """
+        # Get entity list
         e2D = self.ODG_BB_SrvCallback(req)
-
+        # Sort the entity list
+        odg_process = ODG_process()
         score_list, entityList, pitch_list, yaw_list = odg_process.get_e2D_distSorted_withAngles(e2D, self.image_width, self.image_height)
-
+        # Service outputs
         e2D.entity2DList = entityList
-
         return  {   'entities': e2D, #{'header': e2D.header, 'entity2DList': entityList }
                     'pitch_list': pitch_list,
                     'yaw_list' : yaw_list,
-                    'score_list' : score_list
-                }
-
-
+                    'score_list' : score_list }
 
     def img_callback(self, msg_img):
-        if self.ready == 1 :
-            #if self.service_running == 0:
-            self.image_ready = 1
-            self.msg_img = msg_img
-            self.image_width = msg_img.width
-            self.image_height = msg_img.height
+        """
+        Callback on image topic
+        """
+        self.image_ready = 1
+        self.msg_img = msg_img
+        self.image_width = msg_img.width
+        self.image_height = msg_img.height
 
 
 def main():
-    #""" main function
-    #"""
+    """ main function
+    """
     node = ObjectsDetectionGateway_node()
 
 if __name__ == '__main__':
--- darknet_gateway_node/scripts/process/ObjectsDetectionGateway_process.py
+++ darknet_gateway_node/scripts/process/ObjectsDetectionGateway_process.py
@@ -2,148 +2,152 @@
 __author__ = 'Raphael LEBER'
 
 import rospy
+import rospkg
+import csv
 
+from copy import deepcopy
+from math import pi
 
 from darknet_gateway_srvs.srv import ObjectsDetectionGateway_Srv as ODG_Srv
 from darknet_ros_msgs.msg import BoundingBox, BoundingBoxes
-
 from robocup_msgs.msg import Entity2D, Entity2DList
 
-from copy import deepcopy
 
-from math import pi
-
-import csv
-import rospkg
-
-# Node example class.
 class ObjectsDetectionGateway_process():
+    """
+    Sub-class for darknet gateway node.
+    Process operations on detected objects' bounding box.
+    """
 
-    # Must have __init__(self) function for a class, similar to a C++ class constructor.
     def __init__(self):
-
-        self.t = 0
-
+        """
+        create ObjectsDetectionGateway_process object.
+        """
+        # Get the data folder path
         rospack = rospkg.RosPack()
-
-        # get the file path for rospy_tutorials
-        package_path=rospack.get_path('darknet_gateway_node')
+        package_path = rospack.get_path("darknet_gateway_node")
         self.data_folder = package_path + "/data/"
-
-
+        # Get camera FoV
+        self.hfov_deg = rospy.get_param("~/camera/hfov_deg", 57.2)
+        self.vfov_deg = rospy.get_param("~/camera/vfov_deg", 44.3)
 
     def Trad(self):
+        """
+        Open and read the trad.csv file from data folder
 
+        Returns
+        -------
+        dict
+            Dictionnary of labels traduction {darknet class: custom word}
+        """
         with open(self.data_folder + 'trad.csv', mode='r') as infile:
             reader = csv.reader(infile)
-            words = {rows[0]: rows[1] for rows in reader}
+            words_by_classes = {rows[0]: rows[1] for rows in reader}
+        return words_by_classes
 
-        return words
+    def BoundingBoxes_to_Entity2DList(self, bounding_boxes, asked_words):
+        """
+        Make the conversion from BoundingBox message to Entity2DList message
 
+        Parameters
+        ----------
+        bounding_boxes : darknet_ros_msgs.msgs BoundingBoxes
+            List of detected objects in darknet_ros format
+        asked_words : string[]
+            List of objects in which we are interested
 
-    def BoundingBoxes_to_Entity2DList(self, bounding_boxes, labels):
-        el = Entity2DList()
+        Returns
+        -------
+        robocup_msgs.msgs Entity2DList
+            List of detected objects in custom format
 
-        el.header = bounding_boxes.header
-        words = self.Trad()
-
-        print words
-        print " "
-
+        """
+        #Initialise output
+        entity2D_list = Entity2DList()
+        entity2D_list.header = bounding_boxes.header
+        #Get the dictionnary of labels
+        words_by_classes = self.Trad()
+        rospy.loginfo("classes-words traduction {0}".format(words_by_classes))
+        #Through all detected objects
         for bounding_box in bounding_boxes.bounding_boxes :
-
-            if bounding_box.Class in words.keys() :
-                word = words[bounding_box.Class]
+            # Traduction of the labels found by darknet
+            if bounding_box.Class in words_by_classes.keys() :
+                word = words_by_classes[bounding_box.Class]
             else :
                 word = 'There are no object found'
-
-            print word
-
-            if len(labels)==0 or word in labels :
-
-                e = Entity2D()
-
-                e.label =   word
-                #e.xxx =    bounding_box.probability
-                e.pose.x =  (bounding_box.xmin + bounding_box.xmax) / 2
-                e.pose.y =  (bounding_box.ymin + bounding_box.ymax) / 2
-                e.bounding_box.x =      bounding_box.xmin
-                e.bounding_box.y =      bounding_box.ymin
-                e.bounding_box.width =  (bounding_box.xmax - bounding_box.xmin)
-                e.bounding_box.height = (bounding_box.ymax - bounding_box.ymin)
-
-                el.entity2DList.append( e )
-
-        return el
+            rospy.loginfo("{0}".format(word))
+            # Filter out label if needed
+            if (len(asked_words)==0) or (word in asked_words) :
+                entity2D = Entity2D()
+                entity2D.label = word
+                # Go from bounding box corners to center coordinates
+                entity2D.pose.x = (bounding_box.xmin + bounding_box.xmax) / 2
+                entity2D.pose.y = (bounding_box.ymin + bounding_box.ymax) / 2
+                entity2D.bounding_box.x = bounding_box.xmin
+                entity2D.bounding_box.y = bounding_box.ymin
+                entity2D.bounding_box.width =  (bounding_box.xmax - bounding_box.xmin)
+                entity2D.bounding_box.height = (bounding_box.ymax - bounding_box.ymin)
+                # Append to the list
+                entity2D_list.entity2DList.append(entity2D)
+        #Output
+        return entity2D_list
 
 
-    def get_e2D_distSorted_withAngles(self, e2D, image_width, image_height):
+    def get_e2D_distSorted_withAngles(self, entity2D_list, image_width, image_height):
         """
-        Get pitch and yaw of each detected object according to the center of the camera images
-        max pitch =
-        """
-        score_list = []
-        score = 0
+        Get distances score, entities, pitchs and yaws sorted accordingly to the distance score
+        (from the closest to the farthest distance)
 
-        for entity in e2D.entity2DList:
+        Pitch and yaw are computed relativly to the image center.
+
+        Parameters
+        ----------
+        entity2D_list : robocup_msgs.msgs Entity2DList
+            List of detected objects in Entity2D format
+        image_width : int
+            image width
+        image_heigh : int
+            image height
+
+        Returns
+        -------
+        list[4][]
+            distsorted_distscore_list, distsorted_entity2D_list, distsorted_pitch_list, distsorted_yaw_list
+
+        """
+        #Init
+        outputs = []
+        dist_score = 0
+        #Process all detected objects
+        for entity in entity2D_list.entity2DList:
             rospy.loginfo(entity.label)
+            # Bounding box center
             x = entity.bounding_box.x
             y = entity.bounding_box.y
             x2 = x*x #sqrt(x*x, y*y)
-
-            HFov = - 57.2 * pi / 180.0  # Horizontal field of view of the front Pepper Camera
-            Yaw = (HFov / 2.0) *  (x - image_width / 2.0) / (image_width / 2.0) #Angle from the center of the camera to neck_x
-            VFov = - 44.3 * pi / 180.0  # Vertical field of view of the front Pepper Camera
-            Pitch = (VFov / 2.0) *  (y - image_height / 2.0) / (image_height / 2.0) #Angle from the center of the camera to neck_x
-
-            score = float(x2) / float(entity.bounding_box.width) / float(entity.bounding_box.height)
-            score_list.append( [score, entity, Pitch, Yaw] )
-            #find index du min de score_list
-
-        sortedScoreList = sorted(score_list, key=lambda attributes: attributes, reverse=True)
-
-        score_list   = map(lambda x: x[0], sortedScoreList)
-        entityList  = map(lambda x: x[1], sortedScoreList)
-        PitchList   = map(lambda x: x[2], sortedScoreList)
-        YawList     = map(lambda x: x[3], sortedScoreList)
-
-        return score_list, entityList, PitchList, YawList
-
-
-    def BoundingBoxes_Filtered(self, bounding_boxes, labels):
-        bbs = BoundingBoxes()
-
-        bbs.header = bounding_boxes.header
-        words = self.Trad()
-
-        print words
-        print " "
-
-        for bounding_box in bounding_boxes.bounding_boxes :
-
-            if bounding_box.Class in words.keys() :
-                word = words[bounding_box.Class]
-            else :
-                word = 'There are no object found'
-
-            print word
-
-            if len(labels)==0 or word in labels :
-
-                bbs.bounding_boxes.append( bounding_box )
-
-        return bbs
-
-
-    def CreateGoalFromImage(self):
-
-        return 0
-
+            # Compute yaw and pitch from bounding box coordinates
+            hfov = - self.hfov_deg * pi / 180.0  # Horizontal field of view of the front Pepper Camera
+            yaw = (hfov / 2.0) *  (x - image_width / 2.0) / (image_width / 2.0) #Angle from the center of the camera to neck_x
+            vfov = - self.vfov_deg * pi / 180.0  # Vertical field of view of the front Pepper Camera
+            pitch = (vfov / 2.0) *  (y - image_height / 2.0) / (image_height / 2.0) #Angle from the center of the camera to neck_x
+            # Bounding box distance score. Helps to sort detected object from closest to farthest
+            distscore = float(x2) / float(entity.bounding_box.width) / float(entity.bounding_box.height)
+            outputs.append( [distscore, entity, pitch, yaw] )
+        # Sort the output from the closest object to the farthest
+        distsorted_outputs = sorted(outputs, key=lambda x: x[0], reverse=True)
+        # Outputs
+        distsorted_distscore_list = [x[0] for x in distsorted_outputs]
+        distsorted_entity2D_list = [x[1] for x in distsorted_outputs]
+        distsorted_pitch_list = [x[2] for x in distsorted_outputs]
+        distsorted_yaw_list = [x[3] for x in distsorted_outputs]
+        return distsorted_distscore_list, distsorted_entity2D_list, distsorted_pitch_list, distsorted_yaw_list
 
 if __name__ == '__main__':
+    """
+    main
+    """
     try:
         o_p = ObjectsDetectionGateway_process()
         print o_p.Trad()['vienna']
-
     except :#rospy.ROSInterruptException:
         pass
